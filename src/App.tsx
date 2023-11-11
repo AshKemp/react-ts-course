5d4ac8f1ed5bae04f34b10c3e97ebd770c8f3e93
AshKemp <kmuraliashwin@gmail.com>
2023-11-11 10:20:01 +0530

    use useState to store goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalsImg from "../src/assets/goals.jpg";
+import { useState } from "react";
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function addGoalHandler() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your course goals</h1>
       </Header>
-      <CourseGoal title={"Learn React+TS"}>
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      {goals.map((goal) => {
+        return (
+          <ul>
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          </ul>
+        );
+      })}
     </main>
   );
 }
